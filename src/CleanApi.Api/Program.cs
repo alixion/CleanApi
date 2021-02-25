4fbbe291363c701f588f6668049ec0e9b45d6fc7
Alex Manea <ionmanea@gmail.com>
2021-02-25 18:18:59 +0200

    WIP Initial solution structure

---
+++ src/CleanApi.Api/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace CleanApi.Api
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            Log.Information("CleanApi.Api is starting");
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Unhandled exception when starting CleanApi.Api");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            return 0;
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console())
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ src/CleanApi.Api/Sample/SampleEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CleanApi.Api.Sample
+{
+    public class SampleEndpoint:BaseAsyncEndpoint
+        .WithRequest<string>
+        .WithResponse<string>
+    {
+        private readonly ILogger<SampleEndpoint> _logger;
+
+        public SampleEndpoint(ILogger<SampleEndpoint> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet("/hello/{name?}")]
+        [SwaggerOperation(
+            Summary = "Say Hello",
+            Description = "Say Hello",
+            OperationId = "Sample.Hello",
+            Tags = new[] { "HelloEndpoint" })
+        ]
+        public override async Task<ActionResult<string>> HandleAsync(string name, CancellationToken cancellationToken = new CancellationToken())
+        {
+            _logger.LogInformation("Hello {Name}", name);
+            name ??= "world";
+            return Ok($"Hello {name}");
+        }
+    }
+}
